1b2602220142ef8f663d9697386a769691eb9fe0
zhouqinghe <2265441764@qq.com>
2023-12-02 21:14:02 +0800

    :art: 搭架子
    Signed-off-by: zhouqinghe <2265441764@qq.com>

---
+++ service-01-api/src/main/java/com/he/book/dto/BookDTO.java
@@ -0,0 +1,9 @@
+package com.he.book.dto;
+
+/**
+ * @author Ackerman-He
+ * @time 2023/12/2 20:37
+ * @description
+ */
+public class BookDTO {
+}
---
+++ service-01-service/src/main/java/com/he/book/Application.java
@@ -0,0 +1,16 @@
+package com.he.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Ackerman-He
+ * @time 2023/12/2 21:00
+ * @description
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
